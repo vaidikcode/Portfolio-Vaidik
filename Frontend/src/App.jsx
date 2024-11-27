eba981123ea7fc0504f8ed2338bd8d056cf9ddfc
Harkirat Singh <singharkirath1511@gmail.com>
2024-11-27 20:59:26 +0530

    commit2

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Stats from "./components/Stats";
+import Skills from "./components/Skills";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+
+
+
+const App = () => {
+
+
+
+  return (
+    <div className="font-sans">
+      <Header />
+      <Hero />
+      <Stats />
+      <Skills />
+      <Portfolio />
+      <Contact />
+    </div>
+  );
+};
+
+export default App;
