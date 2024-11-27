8acfc4e9b5b5c85483e9f73dbb10524d599f3c7b
vaidikcode <vaidikbhardwaj00@gmail.com>
2024-11-27 19:52:06 +0530

    commit 2

--- Backend/main.go
+++ Backend/main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	"log"
 	"net/http"
 )
 
 func main() {
 	r := mux.NewRouter()
+	c := cors.New(cors.Options{AllowedOrigins: []string{"http://localhost:5173"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},
+		ExposedHeaders:   []string{"Authorization", "Content-Type"},
+		AllowCredentials: true,
+		MaxAge:           86400,
+	})
+	handler := c.Handler(r)
 	r.HandleFunc("/getMessage", getMessage).Methods("POST")
-	err := http.ListenAndServe(":8080", r)
+	err := http.ListenAndServe(":8080", handler)
 	if err != nil {
 		log.Fatal(err)
 	}
